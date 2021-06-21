8111aea44854ce9d0fa6a25e0b021cd9b767d9b8
Lukasz Baldyga <c3ypt1c@gmail.com>
2021-06-21 16:24:31 +0100

    Added some benchmarks

--- Programming/PasswordContainer/src/Pages/CreateContainer.ts
+++ Programming/PasswordContainer/src/Pages/CreateContainer.ts
@@ -1,5 +1,8 @@
 import {} from "./../crypto/Container.js";
 import {$, $$} from "./../DOMHelper.js";
+const Crypto = require("crypto");
+const CryptoJS = require("crypto-js");
+//const CryptoTS = require("crypto-ts"); //TODO: CryptoTS currently breaks, please fix
 
 function calculateMemoryFunction(x : number) { // slider to GB
   // Formula: https://www.desmos.com/calculator/prdhmckdzx
@@ -13,18 +16,35 @@ function calculateMemory() {
   let argon2_memory = $("argon2_memory") as HTMLInputElement;
   let memory_label_string = "We will use {} of memory.";
 
-  let memory_amount = Number.parseFloat(argon2_memory.value);
-  let memory_amount_calculated = calculateMemoryFunction(memory_amount);
+  let memory_amount;
 
-  //console.log("Memory amount: " + memory_amount);
-  //console.log("Memory calculated: " + memory_amount_calculated);
+  if(!($("kdf_argon2") as HTMLInputElement).checked) {
+    // not the argon2 algorithm
+    memory_amount = 1 / 1024;
+  } else if(($("argon2_auto_memory") as HTMLInputElement).checked) {
+    // calculate memory automatically (25, 0.5, 1, 2, 4 & 8 gigabytes)
+    let memoryInGB = (navigator as any).deviceMemory as number; // manual override for TS
 
-  let memory_amount_formatted = memory_amount_calculated > 1 ?
-   Math.round(memory_amount_calculated * 10) / 10 + "Gb" :
-   Math.round(memory_amount_calculated * 1024) + "Mb";
+    // get lowerbound
+    memory_amount = memoryInGB / 2;
+
+  } else {
+    // get memory from slider
+    memory_amount = Number.parseFloat(argon2_memory.value);
+    memory_amount = calculateMemoryFunction(memory_amount);
+  }
+
+  //let memory_amount_calculated = calculateMemoryFunction(memory_amount);
+
+  console.log("Memory calculated: " + memory_amount);
+
+  let memory_amount_formatted = memory_amount > 1 ?
+   Math.round(memory_amount * 10) / 10 + "Gb" :
+   Math.round(memory_amount * 1024) + "Mb";
 
   memory_amount_notifier.textContent = memory_label_string.replace("{}", memory_amount_formatted);
 
+  return memory_amount;
 }
 
 class CreateContainer {
@@ -33,7 +53,7 @@ class CreateContainer {
     $("submitButton").addEventListener("click", this.submitListener);
 
     // Add argon2 specific listeners
-    let argonElements = $$(["kdf_argon2", "kdf_pbudf2", "argon2_auto_memory"]);
+    let argonElements = $$(["kdf_argon2", "kdf_PBKDF2", "argon2_auto_memory"]);
     for(let element = 0; element < argonElements.length; element++) {
       argonElements[element].addEventListener("click", this.argon2_options_listener);
     }
@@ -48,6 +68,8 @@ class CreateContainer {
     this.argon2_options_listener();
     this.time_listener();
     calculateMemory();
+
+    console.log(CryptoJS);
   }
 
   submitListener() {
@@ -59,17 +81,86 @@ class CreateContainer {
 
     // Get KDFs
     let kdf_argon2 = $("kdf_argon2") as HTMLInputElement;
-    let kdf_pbudf2 = $("kdf_pbudf2") as HTMLInputElement;
+    let kdf_PBKDF2 = $("kdf_PBKDF2") as HTMLInputElement;
+
+    // Get memory and time
+    let time = $("time") as HTMLInputElement;
+    let memory = calculateMemory();
 
     // get passwords
     let password_once = $("password_once") as HTMLInputElement;
     let password_twice = $("password_twice") as HTMLInputElement;
 
     // Compare
+    if(password_once.value != password_twice.value) {
+      // TODO: if different throw error
 
-    // if different throw error
+      return;
+    }
 
-    // Make keys and container
+    let password = password_once.value;
+    let salt = Crypto.randomBytes(16);
+    let kdf : "Argon2" | "PBKDF2";
+    const baseIterations = 100_000;
+
+    if(kdf_argon2.checked) kdf = "Argon2";
+    else if(kdf_PBKDF2.checked) kdf = "PBKDF2"
+    else throw "Could not find specificed algorithm";
+
+    // Benchmark TODO: move to worker
+    let start;
+    let end;
+    console.log(CryptoJS);
+    if(kdf == "Argon2") {
+      throw "Not implemented";
+    } else if (kdf == "PBKDF2") {
+      start = performance.now();
+      let optionals = {keySize: 16, iterations: baseIterations};
+      console.log("will submit: ");
+      console.log(password);
+      console.log(salt.toString("base64")); //TODO: Convert to native instead of string
+      console.log(optionals);
+      CryptoJS.PBKDF2(password, salt.toString("hex"), optionals); //https://github.com/brix/crypto-js/blob/develop/src/pbkdf2.js#L120
+      end = performance.now();
+    } else throw "Could not find specificed algorithm";
+
+    // output result
+    let timeTaken = end - start;
+    let targetTime = Number.parseFloat(time.value) * 1000;
+    let timeScale = targetTime / timeTaken;
+    let timeScaledIterations = Math.round(timeScale * baseIterations);
+
+    let result = "Took {time}ms to hash {its} iterations.";
+    result += "\nAssuming {new} ({scale} scale) will take {new_time}ms";
+    result = result.replace("{time}", (Math.round( (timeTaken) * 10) / 10).toString());
+    result = result.replace("{its}", baseIterations.toString());
+    result = result.replace("{new}", timeScaledIterations.toString());
+    result = result.replace("{scale}", timeScale.toString());
+    result = result.replace("{new_time}", targetTime.toString());
+
+    console.log(result);
+
+    // verify result
+    /* if(kdf == "Argon2") {
+      throw "Not implemented";
+    } else */ if (kdf == "PBKDF2") {
+      start = performance.now();
+      let optionals = {keySize: 16, iterations: timeScaledIterations};
+      console.log("will submit: ");
+      console.log(password);
+      console.log(salt.toString("base64")); //TODO: Convert to native instead of string
+      console.log(optionals);
+      CryptoJS.PBKDF2(password, salt.toString("hex"), optionals); //https://github.com/brix/crypto-js/blob/develop/src/pbkdf2.js#L120
+      end = performance.now();
+    } else throw "Could not find specificed algorithm";
+
+    timeTaken = end - start;
+    result = "It took {ms}ms to do {iter} iterations";
+    result = result.replace("{ms}", timeTaken.toString());
+    result = result.replace("{iter}", timeScaledIterations.toString());
+    console.log(result);
+
+    
 
   }
 
