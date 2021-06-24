dc5bf15a3e1a37f3113a7605b9736a70d3f54f2d
Lukasz Baldyga <c3ypt1c@gmail.com>
2021-06-24 03:26:47 +0100

    started identity implementation

--- Programming/PasswordContainer/src/Identity.ts
+++ Programming/PasswordContainer/src/Identity.ts
@@ -1,3 +1,15 @@
-class Identity {
-  
+class Identity implements iJSON {
+  accounts : Account[];
+  identityName : string;
+  identityDesc : string;
+  constructor(identityData : any) {
+    let accounts = [];
+    for(let index = 0; index < identityData["accounts"].length; index++) {
+      new Account(identityData["accounts"][index]);
+    }
+  }
+
+  getJSON() {
+    return "";
+  }
 }
