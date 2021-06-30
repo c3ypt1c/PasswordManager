6b92676e6e33c92006f3eaecc1f2233e80c5be0a
Lukasz Baldyga <c3ypt1c@gmail.com>
2021-06-30 01:26:19 +0100

    now opens with multiple slots confirmed! (Milestone)

--- Programming/PasswordContainer/src/Pages/PasswordManager.ts
+++ Programming/PasswordContainer/src/Pages/PasswordManager.ts
@@ -1,4 +1,5 @@
 import {getStoredContainer, Container} from "./../crypto/Container.js";
+import {Slot, MakeNewSlot} from "./../crypto/Slot.js";
 import {$, $$, disableStatus, goTo} from "./../DOMHelper.js";
 import {Identity} from "./../Identity.js";
 import {log} from "./../crypto/Functions.js";
@@ -24,6 +25,8 @@ export class PasswordManager {
 
       // make elements
 
+      // update settings pane with details
+      updateSettingsPane();
 
       // hide loader
       containerUnlocked();
@@ -32,6 +35,7 @@ export class PasswordManager {
     // Do other housekeeping...
     $("logout").addEventListener("click", this.logout);
     $("change_password").addEventListener("click", this.changePassword);
+    $("add_container").addEventListener("click", this.addContainer);
   }
 
   logout() {
@@ -42,8 +46,8 @@ export class PasswordManager {
   changePassword() {
     log("changing password");
     // get passwords
-    let password_once = $("password_once") as HTMLInputElement;
-    let password_twice = $("password_twice") as HTMLInputElement;
+    let password_once = $("password_change_once") as HTMLInputElement;
+    let password_twice = $("password_change_twice") as HTMLInputElement;
 
     // Compare
     if(password_once.value != password_twice.value) {
@@ -66,6 +70,52 @@ export class PasswordManager {
     });
   }
 
+  addContainer() {
+    log("adding slot to container");
+    // get passwords
+    let password_once = $("password_new_slot_once") as HTMLInputElement;
+    let password_twice = $("password_new_slot_twice") as HTMLInputElement;
+
+    // Compare
+    if(password_once.value != password_twice.value) {
+      // TODO: if different throw error
+
+      return;
+    }
+
+    disableStatus([password_once, password_twice], true);
+    if(container.locked || container.openSlot == null) throw "Container is locked!";
+
+    // get slot data for new slot
+    let slot = container.slots[container.openSlot];
+
+    // make the slot
+    let newSlotPromise = MakeNewSlot(slot.encryptionType, slot.rounds, slot.keyDerivationFunction, slot.getMasterKey(), password_once.value, slot.roundsMemory);
+    newSlotPromise.then((newSlot : Slot) => {
+      // made slot
+      container.slots.push(newSlot);
+      log("Added new slot successfully");
+
+      disableStatus([password_once, password_twice], false);
+    }, (error) => {
+      //failed to make slot
+      //TODO: throw error
+
+      log(error);
+
+      disableStatus([password_once, password_twice], false);
+    });
+  }
+
+}
+
+function updateSettingsPane() {
+  let infoStrings = {
+    "info_slot_open": "Slot open: {}",
+  }
+
+  let slot = container.openSlot == null ? "null" : container.openSlot.toString();
+  $("info_slot_open").textContent = infoStrings["info_slot_open"].replace("{}", slot);
 }
 
 function containerUnlocked() {
