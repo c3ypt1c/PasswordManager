e7ab9ca86cb24eb0fce7a4728cf9b3b9271da584
Lukasz Baldyga <c3ypt1c@gmail.com>
2021-07-02 00:03:47 +0100

    Words correctly process

--- Programming/PasswordContainer/src/Recovery/BIP.ts
+++ Programming/PasswordContainer/src/Recovery/BIP.ts
@@ -2,21 +2,27 @@ import {log} from "./../crypto/Functions.js";
 const fs = require("fs");
 
 export class BIP {
-  words ?: string[];
+  words = [] as string[];
+  wordToNumber = {} as any;
   constructor() {
     //load the words
     fs.readFile("wordlist.txt", "utf8", (error : any, data : string) => {
       log("BIP!");
       log(error);
       log(data);
+
+      // check for errors
+      if(data == null) throw "Data is null";
+
       // sort from smallest to biggest
       let basicWords = data.split("\r\n");
       basicWords.sort((a,b) => a.length - b.length);
 
       // prune bad words
-      this.words = basicWords = basicWords.filter((word) => word.length > 2);
+      basicWords = basicWords.filter((word) => word.length > 2);
       log(basicWords);
 
+      // display stats
       log("number of words: " + basicWords.length);
 
       let bitsPerWord = Math.floor(Math.log2(basicWords.length));
@@ -27,7 +33,21 @@ export class BIP {
       log("useful words:  " + usefulWords);
       log("percent used:  " + percent + "%");
 
+      // select words and process words
+      let averageWordLength = 0;
+      for(let wordNumber = 0; wordNumber < usefulWords; wordNumber++) {
+        let word = basicWords[wordNumber];
+        averageWordLength += word.length / usefulWords;
+        this.words.push(word);
+        
+        //memory/time tradeoff (hashmap O(1) / bin search O(logn))
+        this.wordToNumber[word] = wordNumber;
+      }
 
+      log("made mappings");
+      log(this.words);
+      log(this.wordToNumber);
+      log("average word length: " + (Math.round(10 * averageWordLength) / 10));
     });
   }
 }
