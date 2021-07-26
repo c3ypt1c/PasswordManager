59e19346c51a47c2e0375bcbaf442c9a36d09221
Lukasz Baldyga <c3ypt1c@gmail.com>
2021-07-26 10:48:59 +0100

    New Extra class

--- src/Extra/Extra.ts
+++ src/Extra/Extra.ts
@@ -1,100 +1,12 @@
-/**
- * This class contains the details for the sorted object.
- */
-class ExtraDataSortedObject {
-  identifier : string;
-  dataIndex: number;
-  /**
-   * 
-   * @param identifier Identifier of the data
-   * @param dataIndex  Index for the data
-   */
-  constructor(identifier: string, dataIndex: number) {
-    this.identifier = identifier;
-    this.dataIndex = dataIndex;
-  }
-}
-
-/**
- * Sorted list class of the data
- */
-export class ExtraDataSorted {
-  private sortedData = [] as ExtraDataSortedObject[];
+class ExtraDataMapped extends Map<string, number> {
   constructor(data ?: string[][]) {
-    if(data == null) return; // no data == empty array
-    
+    super();
+    if(!data) return;
+
     for(let index = 0; index < data.length; index++) {
-      let dataObject = data[index]; // get identifier
-      // put the data in a sorted order
-      this.sortedData.push(new ExtraDataSortedObject(dataObject[0], index));
+      this.set(data[index][0], index);
     }
   }
-
-  /**
-   * @param identifier identifier 
-   * @returns the index of the data
-   */
-  getIndex(identifier: string) {
-    let index = this.binarySearch(identifier);
-    return index ? this.sortedData[index].dataIndex : null; 
-  }
-
-  /**
-   * 
-   * @param identifier identifier
-   * @param index index of identifier
-   */
-  addIndex(identifier: string, index: number) {
-    let object = new ExtraDataSortedObject(identifier, index);
-    if(this.sortedData.length == 0) this.sortedData.push(object);
-
-    let sortedIndex = this.bestGuessBinarySearch(identifier);
-    if(sortedIndex == this.sortedData.length) this.sortedData.push(object);
-    
-    let bestGuessObjectIdentifier = this.sortedData[index].identifier;
-
-    if(identifier > bestGuessObjectIdentifier) this.sortedData.splice(index + 1, 0, object); // insert 1 object to the right
-    else if(identifier == bestGuessObjectIdentifier) throw "Cannot add an identifier that already exists";
-    else this.sortedData.splice(index, 0, object);
-  }
-
-  /**
-   * Recursive binary search.
-   * @param identifier string to find
-   * @param start start of search region
-   * @param end end of search region
-   * @returns null if string is not found in `this.sortedData`, otherwise returns the index of the data.
-   */
-   private binarySearch(identifier: string, start ?: number, end ?: number) : number | null {
-    let bestGuess = this.bestGuessBinarySearch(identifier, start, end);
-    return this.sortedData[bestGuess].identifier == identifier ? bestGuess : null;
-  }
-
-  /**
-   * Recursive binary search. Best guess meaning it will return the closest entry 
-   * @param identifier string to find
-   * @param start start of search region
-   * @param end end of search region
-   * @returns the closest match to the identifier.
-   */
-   private bestGuessBinarySearch(identifier: string, start ?: number, end ?: number) : number {
-    start = start ? start : 0;
-    end = end ? end : this.sortedData.length;
-
-    if(end < start) throw "Start is bigger than end";
-
-    // perhaps found exit condition 
-    if(start == end) return start; 
-
-    let middle = Math.floor( (end - start) / 2);
-
-    // bin search
-    let currentIdentifier = this.sortedData[middle].identifier;
-    if(currentIdentifier == identifier) return middle;
-    else if(currentIdentifier > identifier) return this.bestGuessBinarySearch(identifier, start, middle - 1);
-    else return this.bestGuessBinarySearch(identifier, middle + 1, end);
-  }
-
 }
 
 
@@ -104,26 +16,57 @@ export class ExtraDataSorted {
 export class Extra implements iJSON {
   // Because we want to keep the order of the extra data we need to keep the order.
   // eg. data = [["visited", "today"], ["will visit?", "tomorrow"], ["order", "matters"], ... ];
-  public data: string[][];
-  public sortedData : ExtraDataSorted;
+  private data: string[][];
+  private sortedData : ExtraDataMapped;
   constructor(extraData ?: string[][]) {
-    if(extraData == null) {
-      this.data = [];
-      this.sortedData = new ExtraDataSorted();
-    }
-    else {
-      this.data = extraData;
-      this.sortedData = new ExtraDataSorted(extraData);
-    }
+    this.data = extraData ? extraData : [];
+    this.sortedData = new ExtraDataMapped(this.data);
   }
 
+  /**
+   * Sets or updates data. If data exists, it is updated. If not, it is created and added to the end of the list.
+   * @param identifier extra identifier
+   * @param data data to go along with identifier
+   */
   setData(identifier: string, data: string) {
-    //let index = this.sortedData.binarySearch(identifier)
-    //if(index) this.sortedData;
+    let dataObject = [identifier, data];
+
+    if(this.sortedData.has(identifier)) {
+      // has data
+      let index = this.sortedData.get(identifier);
+      if(!index) throw "index is null when this.sortedData has data?";
+
+      this.data[index] = dataObject;
+
+    } else {
+      // no data
+      this.data.push(dataObject);
+      this.sortedData.set(identifier, this.data.length - 1);
+    }
   }
 
   getData(identifier: string) {
+    if(this.sortedData.has(identifier)) {
+      let index = this.sortedData.get(identifier);
+      if(!index) throw "index is null when this.sortedData has data?";
 
+      return this.data[index][1];
+    } 
+    
+    throw "Data with identifier '" + identifier + "' does not exist";
+  }
+
+  hadData(identifier: string) {
+    return this.sortedData.has(identifier);
+  }
+
+  getDataArray() {
+    return this.data;
+  }
+
+  setDataArray(data : string[][]) {
+    this.data = data;
+    this.sortedData = new ExtraDataMapped(data);
   }
 
   getJSON() {
