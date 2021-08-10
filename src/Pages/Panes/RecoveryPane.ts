5add26ca2b78c0a099bb079858e6e30eb18a43e2
Lukasz Baldyga <c3ypt1c@gmail.com>
2021-08-11 00:29:44 +0100

    Recovery pane fixed. Closes #30

--- src/Pages/Panes/RecoveryPane.ts
+++ src/Pages/Panes/RecoveryPane.ts
@@ -5,18 +5,26 @@ import { generateBIPs, ShamirChunk } from "../../Recovery/Shamir.js";
 import { Container } from "./../../Crypto/Container.js";
 import { Pane } from "./Pane.js";
 
-let container : Container;
-let Bip : BIP;
+let container: Container;
+let Bip: BIP;
 
 export class RecoveryPane extends Pane {
-    constructor(container_ : Container, Bip_ : BIP) {
+    constructor(container_: Container, Bip_: BIP) {
         super("recovery_pane", "recovery_pane_button");
         container = container_;
         Bip = Bip_;
 
         // Event Listeners
         $("reveal_bip").addEventListener("click", revealBip);
-        $("generate_shared_recovery").addEventListener("click", createSharedRecovery);
+        $("recovery_pane_generate_shared_recovery").addEventListener("click", createSharedRecovery);
+
+        // up down change
+        $("recovery_pane_pieces").addEventListener("change", sharedRecoveryUpDownEvent);
+        $("recovery_pane_threshold").addEventListener("change", sharedRecoveryUpDownEvent);
+
+        // previous next
+        $("recovery_pane_generate_shared_recovery_previous").addEventListener("click", sharedRecoveryPrevious);
+        $("recovery_pane_generate_shared_recovery_next").addEventListener("click", sharedRecoveryNext);
     }
 
     updatePane(data?: any): void {
@@ -26,131 +34,121 @@ export class RecoveryPane extends Pane {
 
 var bipRevealed = false;
 function revealBip() {
-  if (bipRevealed) return;
-  bipRevealed = true;
+    if (bipRevealed) return;
+    bipRevealed = true;
 
-  let words = Bip.generateFromUint8Array(container.getMasterKey());
-  let bip = $("bip");
-  removeAllChildren(bip);
+    let words = Bip.generateFromUint8Array(container.getMasterKey());
+    let bip = $("bip");
+    removeAllChildren(bip);
 
-  for (let word = 0; word < words.length; word++) {
-    let currentWord = words[word];
+    for (let word = 0; word < words.length; word++) {
+        let currentWord = words[word];
 
-    let bipElement = document.createElement("p");
-    bipElement.classList.add("mx-3");
-    if (currentWord.underlined) bipElement.classList.add("text-decoration-underline");
-    bipElement.textContent = (word + 1) + ". " + currentWord.text;
+        let bipElement = document.createElement("p");
+        bipElement.classList.add("mx-3");
+        if (currentWord.underlined) bipElement.classList.add("text-decoration-underline");
+        bipElement.textContent = (word + 1) + ". " + currentWord.text;
 
-    bip.appendChild(bipElement);
-  }
+        bip.appendChild(bipElement);
+    }
 
-  $("reveal_bip").remove();
+    $("reveal_bip").remove();
 }
 
 function sharedRecoveryUpDownEvent() {
-  log("sharedRecoveryUpDownEvent");
-  let pieces = $("recovery_pieces") as HTMLInputElement;
-  let threshold = $("recovery_threshold") as HTMLInputElement;
-  let piecesValue = Number.parseInt(pieces.value);
-  let thresholdValue = Number.parseInt(threshold.value);
+    log("sharedRecoveryUpDownEvent");
+    let pieces = $("recovery_pane_pieces") as HTMLInputElement;
+    let threshold = $("recovery_pane_threshold") as HTMLInputElement;
+    let piecesValue = Number.parseInt(pieces.value);
+    let thresholdValue = Number.parseInt(threshold.value);
 
-  // ensure minimum
-  piecesValue = piecesValue < 2 ? 2 : piecesValue;
-  thresholdValue = thresholdValue < 2 ? 2 : thresholdValue;
+    // ensure minimum
+    piecesValue = piecesValue < 2 ? 2 : piecesValue;
+    thresholdValue = thresholdValue < 2 ? 2 : thresholdValue;
 
-  // make sure that threshold is at least pieces
-  thresholdValue = thresholdValue > piecesValue ? piecesValue : thresholdValue;
+    // make sure that threshold is at least pieces
+    thresholdValue = thresholdValue > piecesValue ? piecesValue : thresholdValue;
 
-  // return values
-  pieces.value = piecesValue.toString();
-  threshold.value = thresholdValue.toString();
-}
-
-function createSharedRecoveryPane() {
-  // up down change
-  $("recovery_pieces").addEventListener("change", sharedRecoveryUpDownEvent);
-  $("recovery_threshold").addEventListener("change", sharedRecoveryUpDownEvent);
-
-  // previous next
-  $("generate_shared_recovery_previous").addEventListener("click", sharedRecoveryPrevious);
-  $("generate_shared_recovery_next").addEventListener("click", sharedRecoveryNext);
+    // return values
+    pieces.value = piecesValue.toString();
+    threshold.value = thresholdValue.toString();
 }
 
 var shamirChunks = null as null | ShamirChunk[];
 function createSharedRecovery() {
-  // get data and make
-  log("createSharedRecovery");
-  let pieces = $("recovery_pieces") as HTMLInputElement;
-  let threshold = $("recovery_threshold") as HTMLInputElement;
-  let piecesValue = Number.parseInt(pieces.value);
-  let thresholdValue = Number.parseInt(threshold.value);
-  let masterKey = container.getMasterKey();
+    // get data and make
+    log("createSharedRecovery");
+    let pieces = $("recovery_pane_pieces") as HTMLInputElement;
+    let threshold = $("recovery_pane_threshold") as HTMLInputElement;
+    let piecesValue = Number.parseInt(pieces.value);
+    let thresholdValue = Number.parseInt(threshold.value);
+    let masterKey = container.getMasterKey();
 
-  log("mk:");
-  log(masterKey);
-  log("pieces: " + piecesValue);
-  log("thresh: " + thresholdValue);
+    log("mk:");
+    log(masterKey);
+    log("pieces: " + piecesValue);
+    log("thresh: " + thresholdValue);
 
-  shamirChunks = generateBIPs(masterKey, piecesValue, thresholdValue);
-  log(shamirChunks);
+    shamirChunks = generateBIPs(masterKey, piecesValue, thresholdValue);
+    log(shamirChunks);
 
-  // make fields visible
-  $("generate_shared_recovery_screen").classList.remove("d-none");
-  updateRecoveryScreen();
+    // make fields visible
+    $("recovery_pane_generate_shared_recovery_screen").classList.remove("d-none");
+    updateRecoveryScreen();
 }
 
 function updateRecoveryScreen() {
-  checkRecoveryPage();
-  if (shamirChunks == null) throw "shamirChunks are undefined";
-  log("updateRecoveryScreen p:" + page);
+    checkRecoveryPage();
+    if (shamirChunks == null) throw "shamirChunks are undefined";
+    log("updateRecoveryScreen p:" + page);
 
-  // make
-  let shamirChunk = shamirChunks[page];
-  let words = shamirChunk.makeBIP(Bip);
+    // make
+    let shamirChunk = shamirChunks[page];
+    let words = shamirChunk.makeBIP(Bip);
 
-  // assign
-  $("generate_shared_recovery_title").textContent = "Piece number: " + shamirChunk.part.toString();
-  let bipDiv = $("generate_shared_recovery_bip");
-  removeAllChildren(bipDiv);
+    // assign
+    $("recovery_pane_generate_shared_recovery_title").textContent = "Piece number: " + shamirChunk.part.toString();
+    let bipDiv = $("recovery_pane_generate_shared_recovery_bip");
+    removeAllChildren(bipDiv);
 
-  // display
-  for (let word = 0; word < words.length; word++) {
-    let currentWord = words[word];
+    // display
+    for (let word = 0; word < words.length; word++) {
+        let currentWord = words[word];
 
-    let bipElement = document.createElement("p");
-    bipElement.classList.add("mx-3");
-    if (currentWord.underlined) bipElement.classList.add("text-decoration-underline");
-    bipElement.textContent = (word + 1) + ". " + currentWord.text;
+        let bipElement = document.createElement("p");
+        bipElement.classList.add("mx-3");
+        if (currentWord.underlined) bipElement.classList.add("text-decoration-underline");
+        bipElement.textContent = (word + 1) + ". " + currentWord.text;
 
-    bipDiv.appendChild(bipElement);
-  }
+        bipDiv.appendChild(bipElement);
+    }
 }
 
 var page = 0;
 function sharedRecoveryNext() {
-  log("sharedRecoveryNext");
-  page++;
-  updateRecoveryScreen();
+    log("sharedRecoveryNext");
+    page++;
+    updateRecoveryScreen();
 }
 
 function sharedRecoveryPrevious() {
-  log("sharedRecoveryNext");
-  page--;
-  updateRecoveryScreen();
+    log("sharedRecoveryNext");
+    page--;
+    updateRecoveryScreen();
 }
 
 function checkRecoveryPage() {
-  let next = $("generate_shared_recovery_next") as HTMLInputElement;
-  let previous = $("generate_shared_recovery_previous") as HTMLInputElement;
-  previous.disabled = next.disabled = false;
+    let next = $("recovery_pane_generate_shared_recovery_next") as HTMLInputElement;
+    let previous = $("recovery_pane_generate_shared_recovery_previous") as HTMLInputElement;
+    previous.disabled = next.disabled = false;
 
-  if (shamirChunks == null) page = 0;
-  else {
-    page = page < 0 ? 0 : page;
-    page = page >= shamirChunks.length ? shamirChunks.length - 1 : page;
+    if (shamirChunks == null) page = 0;
+    else {
+        page = page < 0 ? 0 : page;
+        page = page >= shamirChunks.length ? shamirChunks.length - 1 : page;
 
-    next.disabled = page == shamirChunks.length - 1; // last page
-    previous.disabled = page == 0; //first page
-  }
+        next.disabled = page == shamirChunks.length - 1; // last page
+        previous.disabled = page == 0; //first page
+    }
 }
 
