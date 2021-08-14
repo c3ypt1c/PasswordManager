97df690a3adf0c7247c473fea60143fedcc93d5d
Lukasz Baldyga <c3ypt1c@gmail.com>
2021-08-14 19:35:44 +0100

    Documentation #23

--- src/iJSON.ts
+++ src/iJSON.ts
@@ -1,3 +1,9 @@
+/**
+ * The interface used to represent objects that can serialise themselfs as JSON.
+ */
 interface iJSON {
+  /**
+   * Return the string JSON representation of the current object.
+   */
   getJSON(): string
 }
