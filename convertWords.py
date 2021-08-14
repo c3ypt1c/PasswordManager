bba09a19f5e7614cbccb14a292bd0f9b0059ad12
Lukasz Baldyga <c3ypt1c@gmail.com>
2021-08-14 04:09:54 +0100

    Added documentation #23

--- convertWords.py
+++ convertWords.py
@@ -1,3 +1,10 @@
+"""
+This file generates the wordlist class /src/Recovery/WordList.ts. It is needed because
+the Recovery works by using the wordlist to assign 16 bits to each word. This program simply 
+makes a valid ts string array containing every word in the wordlist, even if then rejected 
+at runtime.  
+"""
+
 with open("wordlist.txt") as f:
     fd = f.read().split("\n")
 
